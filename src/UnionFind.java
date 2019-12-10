4c167a0459dfcf52bb3f327b744f3b2aa129ffe0
Rajkin-Hossain <rajkin.hossain@gmail.com>
2019-12-11 01:39:36 +0600

    Update UnionFind.java

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -2,18 +2,23 @@ public class UnionFind {
 
 	int [] parent;
 	int [] rank;
+	int [] setSize;
 
 	int size;
-
+	int disjointRoots;
+	
 	public UnionFind(int size) {
 		this.size = size;
 
 		rank = new int[size];
 		parent = new int[size];
+		setSize = new int[size];
 
 		for(int i = 0; i<size; i++) {
 			parent[i] = i;
 		}
+		
+		disjointRoots = size;
 	}
 
 	public void mergeSets(int x, int y) {
@@ -21,15 +26,27 @@ public class UnionFind {
 		int PY = findSet(y);
 
 		if(PX == PY) return;
+		
+		disjointRoots--;
 
 		if (rank[PX] > rank[PY]) {
-			parent[PY] = PX; // means py er parent ekhon px
+			parent[PY] = PX;
+			setSize[PX] = setSize[PX] + setSize[PY];
 		}
 		else {
 			parent[PX] = PY;
+			setSize[PY] = setSize[PX] + setSize[PY];
 		}
 
 		if (rank[PX] == rank[PY]) rank[PY] = rank[PY] + 1; // 2 ta rank soman thaka means ekta tree er size barano
+	}
+	
+	public int sizeOfSet(int i) { 
+		return setSize[findSet(i)]; 
+	}
+	
+	public int numOfDisjointSets() { 
+		return disjointRoots; 
 	}
 
 	public int findSet(int x){
